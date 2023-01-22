654ee11d16134e07b3132eb9c3132d05f830f4de
Danilo Silva <danilo.silva@msn.com>
2023-01-22 09:09:15 -0300

    Definindo a Entidade Base

---
+++ Todo/Todo.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class BaseEntity : IEquatable<BaseEntity>
+    {
+        protected BaseEntity(Guid id)
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals(BaseEntity other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
