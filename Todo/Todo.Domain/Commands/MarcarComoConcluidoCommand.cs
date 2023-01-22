b18a27f807538410007f2b274974e5fe1a200d4e
Danilo Silva <danilo.silva@msn.com>
2023-01-22 09:46:56 -0300

    Marcar Item como Concluído

---
+++ Todo/Todo.Domain/Commands/MarcarComoConcluidoCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class MarcarComoConcluidoCommand : Notifiable, ICommand
+    {
+        public MarcarComoConcluidoCommand() { }
+
+        public MarcarComoConcluidoCommand(Guid id, string usuario)
+        {
+            Id = id;
+            Usuario = usuario;
+        }
+
+        public Guid Id { get; set; }
+        public string Usuario { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                    .Requires()
+                    .HasMinLen(Usuario, 6, "Usuario", "Usuário inválido!")
+            );
+        }
+    }
+}
