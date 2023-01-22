9612130dd2a03a09685f982136658122df619554
Danilo Silva <danilo.silva@msn.com>
2023-01-22 09:23:20 -0300

    Estrutura dos Commands

---
+++ Todo/Todo.Domain/Commands/CreateCommandTodo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateCommandTodo : ICommand
+    {
+        public CreateCommandTodo() { }
+
+        public CreateCommandTodo(string titulo, DateTime data, string usuario)
+        {
+            Titulo = titulo;
+            Data = data;
+            Usuario = usuario;
+        }
+
+        public string Titulo { get; set; }
+        public DateTime Data { get; set; }
+        public string Usuario { get; set; }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
