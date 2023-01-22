8e37c6cbb111d35d8495e0ae18f68f7229cac236
Danilo Silva <danilo.silva@msn.com>
2023-01-22 20:04:43 -0300

    Testando o Command

--- Todo/Todo.Domain.Tests/Command/CriarTarefaCommandTests.cs
+++ Todo/Todo.Domain.Tests/Command/CriarTarefaCommandTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Commands;
 
 namespace Todo.Domain.Tests.Command
 {
@@ -10,13 +12,17 @@ namespace Todo.Domain.Tests.Command
         [TestMethod]
         public void Dado_Um_Comando_Invalido()
         {
-            Assert.Fail();
+            var command = new CriarTarefaCommand("", DateTime.Now, "");
+            command.Validate();
+            Assert.AreEqual(command.Valid, false);
         }
 
         [TestMethod]
         public void Dado_Um_Comando_Valido()
         {
-            Assert.Fail();
+            var command = new CriarTarefaCommand("Título da Tarefa", DateTime.Now, "Danilo Silva");
+            command.Validate();
+            Assert.AreEqual(command.Valid, true);
         }
     }
 }
