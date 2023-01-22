78dbaea9644df054fba2f5e5aea1420fe6200dff
Danilo Silva <danilo.silva@msn.com>
2023-01-22 19:16:06 -0300

    Comaçando o Handler

---
+++ Todo/Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers
+{
+    public class TodoHandler : Notifiable, IHandler<CriarTarefaCommand>
+    {
+        private readonly ITodoRepository _repository;
+
+        public TodoHandler(ITodoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(CriarTarefaCommand command)
+        {
+            
+        }
+    }
+}
