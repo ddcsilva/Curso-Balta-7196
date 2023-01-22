8cdb2401d49556225d5dfc468003312335cab6c6
Danilo Silva <danilo.silva@msn.com>
2023-01-22 19:12:01 -0300

    Contrato do Todo Repository

---
+++ Todo/Todo.Domain/Repositories/ITodoRepository.cs
@@ -0,0 +1,10 @@
+﻿using Todo.Domain.Entities;
+
+namespace Todo.Domain.Repositories
+{
+    public interface ITodoRepository
+    {
+        void Inserir(Item item);
+        void Atualizar(Item item);
+    }
+}
