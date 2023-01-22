971bdda7adf95be62ae3edbcb7a5017d316b9fb0
Danilo Silva <danilo.silva@msn.com>
2023-01-22 19:57:34 -0300

    Testes de Unidade

---
+++ Todo/Todo.Domain.Tests/Command/CriarTarefaCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Todo.Domain.Tests.Command
+{
+    [TestClass]
+    public class CriarTarefaCommandTests
+    {
+        // Red, Green, Refactor
+
+        [TestMethod]
+        public void Dado_Um_Comando_Invalido()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Dado_Um_Comando_Valido()
+        {
+            Assert.Fail();
+        }
+    }
+}
