5a68a216f88fa37f4b298a6dac7bcf92e701a6e4
Danilo Silva <danilo.silva@msn.com>
2023-01-23 08:59:35 -0300

    Alterando os Mapeamentos

--- Todo/Todo.Domain.Infra/Contexts/DataContext.cs
+++ Todo/Todo.Domain.Infra/Contexts/DataContext.cs
@@ -8,5 +8,16 @@ namespace Todo.Domain.Infra.Contexts
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
         public DbSet<Tarefa> Tarefas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tarefa>().ToTable("Todo");
+            modelBuilder.Entity<Tarefa>().Property(x => x.Id);
+            modelBuilder.Entity<Tarefa>().Property(x => x.Usuario).HasMaxLength(120).HasColumnType("varchar(120)");
+            modelBuilder.Entity<Tarefa>().Property(x => x.Titulo).HasMaxLength(160).HasColumnType("varchar(160)");
+            modelBuilder.Entity<Tarefa>().Property(x => x.Concluido).HasColumnType("bit");
+            modelBuilder.Entity<Tarefa>().Property(x => x.Concluido);
+            modelBuilder.Entity<Tarefa>().HasIndex(b => b.Concluido);
+        }
     }
 }
