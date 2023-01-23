d65ac8ccfd99d0a936513d7fb730ad8c1299527e
Danilo Silva <danilo.silva@msn.com>
2023-01-23 09:26:10 -0300

    Todo Queries

---
+++ Todo/Todo.Domain/Queries/TodoQueries.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+using System;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Queries
+{
+    public static class TodoQueries
+    {
+        public static Expression<Func<Tarefa, bool>> RetornarTodasTarefas(string usuario)
+        {
+            return x => x.Usuario == usuario;
+        }
+
+        public static Expression<Func<Tarefa, bool>> RetornarTodasTarefasConcluidas(string usuario)
+        {
+            return x => x.Usuario == usuario && x.Concluido == true;
+        }
+
+        public static Expression<Func<Tarefa, bool>> RetornarTodasTarefasNaoConcluidas(string usuario)
+        {
+            return x => x.Usuario == usuario && x.Concluido == false;
+        }
+
+        public static Expression<Func<TodoItem, bool>> RetornarTodasTarefasPorPeriodo(string usuario, DateTime data, bool concluido)
+        {
+            return x =>
+                x.User == usuario &&
+                x.Done == concluido &&
+                x.Date.Date == data.Date;
+        }
+    }
+}
