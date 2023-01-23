862a5b3d93a3ce27a52ebac982c04b49872b6fb9
Danilo Silva <danilo.silva@msn.com>
2023-01-23 13:51:31 -0300

    Estrutura do Repositório

--- Todo/Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
+++ Todo/Todo.Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using Todo.Domain.Entities;
 using Todo.Domain.Repositories;
-using Todo.Domain.Tests.EntityTests;
 
 namespace Todo.Domain.Tests.Repositories
 {
@@ -9,17 +9,37 @@ namespace Todo.Domain.Tests.Repositories
     {
         public void Inserir(Tarefa tarefa)
         {
-
+            throw new NotImplementedException();
         }
 
         public void Atualizar(Tarefa tarefa)
         {
-
+            throw new NotImplementedException();
         }
 
         public Tarefa BuscarPorId(Guid id, string usuario)
         {
             return new Tarefa("Título da Tarefa", DateTime.Now, "danilosilva");
+        }
+
+        public IEnumerable<Tarefa> RetornarTodas(string usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tarefa> RetornarTodasConcluidas(string usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tarefa> RetornarTodasNaoConcluidas(string usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tarefa> RetornarTodasPorPeriodo(string usuario, DateTime data, bool concluido)
+        {
+            throw new NotImplementedException();
         }
     }
 }
