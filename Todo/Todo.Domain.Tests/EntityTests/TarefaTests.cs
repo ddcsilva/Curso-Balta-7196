b269c7eeb9d25092afd2a66670735ce946339a5c
Danilo Silva <danilo.silva@msn.com>
2023-01-23 07:14:20 -0300

    Testando a Entidade

---
+++ Todo/Todo.Domain.Tests/EntityTests/TarefaTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly Tarefa _validTodo = new Tarefa("Titulo Aqui", DateTime.Now, "danilosilva");
+
+        [TestMethod]
+        public void Dado_Um_Novo_Todo_O_Mesmo_Nao_Pode_Ser_Concluido()
+        {
+            Assert.AreEqual(_validTodo.Concluido, false);
+        }
+    }
+}
