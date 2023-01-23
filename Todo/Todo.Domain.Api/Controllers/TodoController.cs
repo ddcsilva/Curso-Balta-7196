30452de6665b7278c2a629cbdaea96d706cde6b8
Danilo Silva <danilo.silva@msn.com>
2023-01-23 19:57:26 -0300

    Criando uma Tarefa

---
+++ Todo/Todo.Domain.Api/Controllers/TodoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Domain.Commands;
+using Todo.Domain.Handlers;
+
+namespace Todo.Domain.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult CriarTarefa(
+            [FromBody] CriarTarefaCommand command,
+            [FromServices] TarefaHandler handler
+        )
+        {
+            command.Usuario = "danilosilva";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+    }
+}
