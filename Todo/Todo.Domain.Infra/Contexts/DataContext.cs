0b2d5652cf6462da1ae0997e25e3d5c97fe86138
Danilo Silva <danilo.silva@msn.com>
2023-01-23 08:37:15 -0300

    Data Context

---
+++ Todo/Todo.Domain.Infra/Contexts/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Infra.Contexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
